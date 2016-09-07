84d5cdb3e1f4bd48bbd015b3f4c6e2642389fc3c
elliotwhiley <elliot.whiley94@gmail.com>
2016-09-07 15:41:09 +1200

    Revert "Apologies to Github"
    This reverts commit c738ed5a957c47b3ac4601b2ed162b2463ceecfb.

--- game.py
+++ game.py
@@ -1,4 +1,3 @@
 The final file which is going to be added
 Github Desktop sucks
 It's my third time saying this. Github Desktop sucks
-Actually, I'm sorry Github T_T my name is Harvey btw. 4th year softeng student at UoA
